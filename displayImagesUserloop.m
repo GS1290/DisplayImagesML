fa5639f3560cedd06e0b6ac6d7543f91df81443d
GS1290 <shashank.gouroju@gmail.com>
2023-07-26 19:55:57 +0530

    Display 3 random images per trial - First version

--- displayImagesUserloop.m
+++ displayImagesUserloop.m
@@ -1,92 +1,5 @@
 function [C,timingfile,userdefined_trialholder] = displayImagesUserloop(MLConfig,TrialRecord)
 
-% A userloop file is a MATLAB function that provides information of the
-% next trial (stimuli, timing file name, condition number, block number,
-% etc.), instead of the conditions file, the condition selection file,
-% the block selection file and the error handling logic. It helps users
-% compose a task in a more flexible way, without being restricted by the
-% conditions file structure.
-%
-% Users can load a userloop file (*.m) from the conditions file selection
-% dialog in the main menu, as when loading a normal conditions file. The
-% conditions file dialog looks for text files (*.txt), by default, so,
-% to make things easier, you can make a text file, write the name of the
-% userloop file in it and choose that text file instead.
-%
-% When a userloop file is loaded, the main menu displays nothing but
-% 'user-defined' in the stimulus list box and the timing files box, but the
-% run button becomes enabled so that you can start the task. Then, it is
-% the userloop's job to supply stimuli and timing files.
-
-% A userloop function has two input arguments, MLConfig and TrialRecord.
-% MLConfig has the current settings of ML and TrialRecord keeps the numbers
-% related to the current trial and the performance history.
-% Note that the numbers in TrialRecord are not updated for the next trial
-% until the userloop is finished. For example, while in the userloop,
-% TrialRecord.CurrentTrialNumber is 0 although the trial that we are about
-% to execute is Trial 1.
-%
-% Another important thing to know is that the userloop is called twice
-% before Trial 1 starts: once before the pause menu shows up (MonkeyLogic
-% does that, to retrieve the timingfile name(s)) and once immediately before
-% Trial 1. So be careful, when writing the code, not to waste your preset
-% conditions in the very first call. See the code below for a trick to
-% avoid this issue.
-
-% Set the block number and the condition number of the trial, like the
-% following. These numbers are just for users' information and ML does not
-% use them, since this userloop determined the stimulus list and the timing
-% script.
-% If you assign -1 to TrialRecord.NextBlock, the task will be aborted
-% without running the next trial.
-%
-%   TrialRecord.NextBlock = no;
-%   TrialRecord.NextCondition = no;
-%
-% It is no longer allowed to add new fields to TrialRecord directly. Use
-% the 'User' field instead. This is because TrialRecord is now a MATLAB
-% class object, not a struct.
-%
-%   TrialRecord.NewField = 1;       % error
-%   TrialRecord.User.NewField = 1;  % good
-
-% In the conditions file, some extra information of the condition can be
-% delievered with the Info field.
-%
-%   Condition   Block   Frequency   Timing File     Info
-%   1           1       1           grating         'deg',45
-%
-% To deliver the same information using the userloop, create a struct
-% yourself and call the TrialRecord.setCurrentConditionInfo(struct)
-% function.
-%
-%   a.deg = 45;
-%   TrialRecord.setCurrentConditionInfo(a);
-
-% Three output arguments of the userloop function are the taskobjects ("C"),
-% the timing file name and the user-defined trialholder name.
-%
-% The taskobjects can be specified in two ways. One is to use a cell char
-% array and the other is to use a struct array. Refer to the following
-% examples.
-%
-% ----- a cell char array example -----
-% C = { 'fix(0,0)', 'pic(A.jpg,0,0,320,240,[0 0 0])', 'mov(B.avi,0,0)', ...
-%       'crc(0.5,[1 0 0],1,-7,0)', 'sqr([0.6 0.3],[0 0 1],1,7,0)', ...
-%       'snd(C.wav)', 'stm(2,D.mat,1)', 'ttl(3)', 'gen(E.m,0,0)' };
-%
-
-% The user-defined trialholder is not necessary for most of users, so just
-% leave it empty.
-
-% This example task runs two blocks of a delayed match-to-sample task.
-% Block 1 uses A.BMP and B.BMP as stimuli and Block 2, C.BMP and D.BMP.
-% Each block has 4 possible conditions, depending on which image becomes
-% the sample and where the sample is presented (left or right).
-% The order of the conditions is randomized (with replacement) and an error
-% trials is repeated immediately. The blocks are alternated every 20
-% correct responses.
-
 % default return value
 C = [];
 timingfile = 'DisplayImagesTiming.m';
@@ -111,31 +24,43 @@ if 0==TrialRecord.CurrentTrialNumber
     numImg = length(image_list);
 end
 
-
 block = TrialRecord.CurrentBlock;
 condition = TrialRecord.CurrentCondition;
 
-% Set a new condition if this is the first trial or the last trial was
-% answered correctly. If the last trial failed, then the inside of the IF
-% statement is skipped so the same condition is repeated.
+% Initialize the number of conditions for a block
+persistent borrow_conditions
 persistent condition_sequence
-if isempty(TrialRecord.TrialErrors) || 0==TrialRecord.TrialErrors(end)
-    correct_trial_count = sum(0==TrialRecord.TrialErrors);
-    
-    % Increase the block number after 20 correct trials
-    block = mod(floor(correct_trial_count/(numImg)),2) + 1;
-    
-    % Select the next condition randomly
-    condition_index = mod(correct_trial_count,numImg) + 1;
-    if 1==condition_index, condition_sequence = randperm(numImg); end
-    condition = condition_sequence(condition_index);
+persistent prev_conditions
+if isempty(condition_sequence)
+    condition_sequence = 1:numImg;
+    condition_sequence = setdiff(condition_sequence, borrow_conditions);
+    block = block + 1;
 end
 
+if isempty(TrialRecord.TrialErrors)
+    condition = condition+1;
+% If the last trial is a success, remove those conditions from the sequence
+elseif ~isempty(TrialRecord.TrialErrors) && 0==TrialRecord.TrialErrors(end)
+    condition_sequence = setdiff(condition_sequence, prev_conditions);
+    condition = condition+3;
+end
+
+% If there are more than 2 conditions left to show in the current block,
+% sample 3 conditions
+if length(condition_sequence)>=3
+    condition_indices = datasample(condition_sequence, 3, 'Replace',false);
+    prev_conditions = condition_indices;
+else
+    prev_conditions = condition_sequence;
+    borrow_conditions = datasample(1:numImg, 3-length(condition_sequence), 'Replace', false);
+    condition_indices = [condition_sequence borrow_conditions];
+    condition_indices = condition_indices(randperm(3));
+end
 
 % Set the stimuli
-stimuli1 = fullfile('Images', image_list{condition});
-stimuli2 = fullfile('Images', image_list{condition});
-stimuli3 = fullfile('Images', image_list{condition});
+stimuli1 = fullfile('Images', image_list{condition_indices(1)});
+stimuli2 = fullfile('Images', image_list{condition_indices(2)});
+stimuli3 = fullfile('Images', image_list{condition_indices(3)});
 
 C = { 'fix(0,0)', ...
     sprintf('pic(%s,0,0)',stimuli1), ...
